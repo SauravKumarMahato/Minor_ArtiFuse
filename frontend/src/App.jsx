e8ca15e2a9bfde05b3880ddac72a0e1efde01859
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2024-02-14 22:10:53 +0545

    setup tailwind , works in App.jsx etc.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import AppRoute from './components/routes/AppRoute'
+
+function App() {
+
+  return (
+    <>
+
+      <AppRoute />
+
+    </>
+  )
+}
+
+export default App
